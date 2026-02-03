d786701900d69e2feb3b3b0f33ffeca0ed237160
Sergio Díaz Prados <97954632+Radiomical@users.noreply.github.com>
2026-02-03 11:42:42 +0100

    Update urls.py
    cambios información sensible

--- portal/urls.py
+++ portal/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('s3c-panel-secret0/', admin.site.urls),
+    path('xxx/', admin.site.urls),
     path('', include(('blog.urls', 'blog'), namespace='blog')),
     path('users/', include(('users.urls', 'users'), namespace='users')),
     path('api/', include(('api.urls', 'api'), namespace='api')),
@@ -10,5 +10,6 @@ urlpatterns = [
 
 from django.conf.urls.static import static
 from django.conf import settings
+
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
